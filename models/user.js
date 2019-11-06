99975d8548910e87bdf97a93c6ea1cea9098bacd
Ckbhatia <chetansain86@gmail.com>
2019-11-06 21:13:34 +0530

    fix: add timestamps object to schema

--- models/user.js
+++ models/user.js
@@ -3,18 +3,21 @@ const mongoose = require("mongoose");
 // Define a schema
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({
-  title: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  picture: String,
-  email: { type: String, required: true, unique: true },
-  contact: { type: Number, required: true, unique: true },
-  description: { type: String, required: true },
-  address: String,
-  password: { type: String, required: true, minlength: 6 },
-  // TODO: Add post field and reference it to another schema by id
-  post: [{ type: Schema.Types.ObjectId, ref: "Post" }]
-});
+const userSchema = Schema(
+  {
+    title: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    picture: String,
+    email: { type: String, required: true, unique: true },
+    contact: { type: Number, required: true, unique: true },
+    description: { type: String, required: true },
+    address: String,
+    password: { type: String, required: true, minlength: 6 },
+    // TODO: Add post field and reference it to another schema by id
+    post: [{ type: Schema.Types.ObjectId, ref: "Post" }]
+  },
+  { timestamps: true }
+);
 
 // Create user instace
 const User = mongoose.model("User", userSchema);
