31aba2c23a6f6d2a82356af55ac7960aa8a3dbe8
Ckbhatia <chetansain86@gmail.com>
2019-11-06 21:18:05 +0530

    feat: add post schema

---
+++ models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const postSchema = Schema(
+  {
+    description: { type: String, required: true },
+    media: String,
+    location: String,
+    author: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }]
+  },
+  { timestamps: true }
+);
+
+// Create post instace
+const Post = mongoose.model("Post", postSchema);
+
+// Export the post model
+module.exports = Post;
