a625cadc6393c607d9c64c5a403b416c61eb6fb3
Ckbhatia <chetansain86@gmail.com>
2019-11-07 13:02:11 +0530

    feat: connect to mongoose databse

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
+const mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
+
+// Connect mongoose
+mongoose.connect(
+  process.env.MONGOURL || "mongodb://localhost/conduit_api",
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  err => {
+    err ? console.log(err) : console.log("success mongodb connected");
+  }
+);
 
 var app = express();
 
