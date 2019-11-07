0778405771d102a086e5ce63b48578bf609eaae5
Ckbhatia <chetansain86@gmail.com>
2019-11-07 13:05:23 +0530

    fix: remove connect mongoose code

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const mongoose = require("mongoose");
-
-// Connect mongoose
-mongoose.connect(
-  process.env.MONGOURL || "mongodb://localhost/conduit_api",
-  { useUnifiedTopology: true, useNewUrlParser: true },
-  err => {
-    err ? console.log(err) : console.log("success mongodb connected");
-  }
-);
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
